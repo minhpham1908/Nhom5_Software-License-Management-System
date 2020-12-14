1dd900fee98dc9ff480c50ee39e10ddc20af63f8
lvquang98vp <lv.quang.98.vp@gmail.com>
2020-12-14 22:01:18 +0700

    Update licence with validThoght and products

--- RMIService/src/yuhnim/rmi/Licence.java
+++ RMIService/src/yuhnim/rmi/Licence.java
@@ -63,6 +63,29 @@ public class Licence implements Serializable {
         return this;
     }
 
+    //Add More
+
+    public void setTitleAndlicenceID(String title, Integer licenceID){
+        // Gan gia tri Title
+        // Title co gia tri mac dinh la string
+        this.title = title;
+
+        // Gan gia tri licenceID
+        // licenceID co gia tri mac dinh la integer
+        this.licenceID = licenceID;
+    }
+
+    public void setValidThoughAndProductName(Date validThough, ArrayList<Product> products){
+        // Gan gia tri cho validThogh
+        // validThough co kieu du lieu mac dinh la Date
+        this.validThough = validThough;
+
+        // Gan gia tri cho products
+        // products co gia tri mac dinh la mot mang bao gom list cac product
+        this.products = products;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (o == this)
