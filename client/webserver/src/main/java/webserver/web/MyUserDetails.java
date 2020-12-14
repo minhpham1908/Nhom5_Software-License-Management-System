74a694c771810f8d6c309cd20c85ec957d6405f1
minhpham1908 <huyminhh1998@gmail.com>
2020-12-15 00:54:25 +0700

    Custom file for Security

---
+++ client/webserver/src/main/java/webserver/web/MyUserDetails.java
@@ -0,0 +1,24 @@
+package webserver.web;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserDetails extends User {
+    private Integer userId;
+    public MyUserDetails(Integer userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.userId=userId;
+    }
+
+    public MyUserDetails(Integer userId,String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.userId=userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+}
---
+++ client/webserver/src/main/java/webserver/web/service/UserService.java
@@ -0,0 +1,66 @@
+package webserver.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.util.Assert;
+import webserver.web.MyUserDetails;
+import yuhnim.client.Client;
+import yuhnim.rmi.User;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("userDetailService")
+public class UserService implements UserDetailsService {
+    @Autowired
+    Client client;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //
+        User user = null;
+//        UserBuilder builder = null;
+        try {
+            user = findUserByUserName(username);
+//            builder = org.springframework.security.core.userdetails.User.withUsername(user.getName());
+//            builder.password(user.getEncryptedPassword());
+//            builder.roles("USER");
+            System.out.println(user);
+        } catch (RemoteException e) {
+            System.out.println("REMOTE EXCEPTION: " + e.toString());
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            System.out.println("NOT BOUND EXCEPTION: " + e.toString());
+            e.printStackTrace();
+        }
+        MyUserDetails myUserDetails = new MyUserDetails(user.getID(), user.getName(), user.getEncryptedPassword(), new ArrayList(dumbGetRoles("USER")));
+        return myUserDetails;
+    }
+
+    private User findUserByUserName(String userName) throws RemoteException, NotBoundException {
+        return client.getUserByUsername(userName);
+    }
+
+    private List<GrantedAuthority> dumbGetRoles(String... roles) {
+        List<GrantedAuthority> authorities = new ArrayList(roles.length);
+        String[] var3 = roles;
+        int var4 = roles.length;
+
+        for (int var5 = 0; var5 < var4; ++var5) {
+            String role = var3[var5];
+            Assert.isTrue(!role.startsWith("ROLE_"), () -> {
+                return role + " cannot start with ROLE_ (it is automatically added)";
+            });
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        }
+        return authorities;
+    }
+}
