58482e7ee753faf6e5a2e1f4a25ebe936e1c61c5
minhpham1908 <huyminhh1998@gmail.com>
2020-12-15 00:53:19 +0700

    Implementing code cause I fdumb

--- server/src/yuhnim/server/UserServiceImp.java
+++ server/src/yuhnim/server/UserServiceImp.java
@@ -1,15 +1,23 @@
 package yuhnim.server;
 
-import yuhnim.rmi.AuthorizationService;
 import yuhnim.rmi.User;
 import yuhnim.rmi.UserService;
+import yuhnim.server.DAL.DAO;
+import yuhnim.server.DAL.UserDAO;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class UserServiceImp implements UserService {
+public class UserServiceImp extends UnicastRemoteObject implements UserService {
+    private static final long serialVersionUID = 1L;
+
+    protected UserServiceImp() throws RemoteException {
+    }
 
     @Override
     public User getUser(String username) throws RemoteException {
-        return null;
+        UserDAO userDAO = new UserDAO();
+        User user = userDAO.get(username);
+        return user;
     }
 }
