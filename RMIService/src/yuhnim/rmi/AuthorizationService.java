953841650063898a95c40e253e0c7c1c55a78f0e
minhpham1908 <huyminhh1998@gmail.com>
2020-12-15 00:52:47 +0700

    Clean code

--- RMIService/src/yuhnim/rmi/AuthorizationService.java
+++
@@ -1,8 +0,0 @@
-package yuhnim.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AuthorizationService extends Remote {
-    public boolean authorize(User user) throws RemoteException;
-}
