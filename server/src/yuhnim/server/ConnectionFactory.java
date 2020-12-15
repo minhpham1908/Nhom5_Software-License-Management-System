dfcba7654e78da306752e8aab87ad1caa859ebf2
minhpham1908 <huyminhh1998@gmail.com>
2020-12-16 00:20:52 +0700

    Docker config

--- server/src/yuhnim/server/ConnectionFactory.java
+++ server/src/yuhnim/server/ConnectionFactory.java
@@ -6,12 +6,13 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
     private static String DB_URL = "jdbc:mysql://localhost:3306/software-licence";
+    private static String DB_DOCKER_URL = "jdbc:mysql://host.docker.internal:3306/software-licence";
     private static String USER_NAME = "root";
     private static String PASSWORD = "madafakas";
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            return DriverManager.getConnection(DB_DOCKER_URL, USER_NAME, PASSWORD);
         } catch (Exception ex) {
             System.out.println("connect failure!");
             throw new RuntimeException("connect failure!", ex);
